8299a79b46795b5c9ef2190d40e3ca1f6033917e
zh6335901 <zh6335901@hotmail.com>
2022-12-08 23:20:01 +0800

    Day 3

---
+++ AdventOfCode2022/Day3/Day3.fs
@@ -0,0 +1,36 @@
+﻿module Day3
+
+open System.IO
+
+module private TestData = 
+    let input = File.ReadAllLines("Day3/input.txt");
+
+let getPriority (c: char) =
+    if System.Char.IsUpper c 
+    then (int c) - (int 'A') + 27
+    else (int c) - (int 'a') + 1
+
+let strToCharSet (str: string) =
+    str.ToCharArray() |> Set.ofArray
+
+module Puzzle5 = 
+    let solve (lines: string array) = 
+        lines
+        |> Array.map (fun l -> let cp = l.Length / 2 in (l.Substring(0, cp), l.Substring(cp)))
+        |> Array.map (fun a -> strToCharSet (fst a), strToCharSet (snd a))
+        |> Array.map (fun x -> (fst x) |> Set.intersect (snd x))
+        |> Array.map (fun s -> s |> Set.toSeq |> Seq.sumBy getPriority)
+        |> Array.sum
+
+    let result = solve TestData.input
+
+module Puzzle6 = 
+    let solve (lines: string array) =
+        lines
+        |> Array.chunkBySize 3
+        |> Array.map (fun g -> g |> Array.map strToCharSet)        
+        |> Array.map (fun x -> x |> Array.reduce (fun s i -> Set.intersect s i))
+        |> Array.map (fun s -> s |> Set.toSeq |> Seq.sumBy getPriority)
+        |> Array.sum
+
+    let result = solve TestData.input
--- AdventOfCode2022/Program.fs
+++ AdventOfCode2022/Program.fs
@@ -1,8 +1,12 @@
 ﻿open Day1
 open Day2
+open Day3
 
 printfn "%d" Puzzle1.result
 printfn "%d" Puzzle2.result
 
 printfn "%d" Puzzle3.result
 printfn "%d" Puzzle4.result
+
+printfn "%d" Puzzle5.result
+printfn "%d" Puzzle6.result
