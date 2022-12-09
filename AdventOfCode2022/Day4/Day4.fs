e89961d1c3970c464a0cb544e46bd173f100a2b6
zh6335901 <zh6335901@hotmail.com>
2022-12-09 22:44:44 +0800

    Day 4

---
+++ AdventOfCode2022/Day4/Day4.fs
@@ -0,0 +1,50 @@
+﻿module Day4
+
+open System.IO
+
+module private TestData = 
+    let input = File.ReadAllLines("Day4/input.txt")
+
+type Range = int * int
+type Pair = Range * Range
+
+let parse (line: string): Pair =
+    let parseRange (str: string) =
+        let rangeArr = str.Split("-")
+        (int rangeArr[0], int rangeArr[1])
+
+    let pairArr = line.Split(",")
+    (parseRange pairArr[0], parseRange pairArr[1])
+
+let countTrue (bs: bool array) =
+    bs |> Array.sumBy (fun b -> if b then 1 else 0)
+
+module Puzzle7 = 
+    let fullyContains (pair: Pair) = 
+        match pair with
+        | (l1, r1), (l2, r2) when l1 >= l2 && r1 <= r2 -> true
+        | (l1, r1), (l2, r2) when l2 >= l1 && r2 <= r1 -> true
+        | _ -> false
+
+    let solve (lines: string array) = 
+        lines
+        |> Array.map parse
+        |> Array.map fullyContains
+        |> countTrue
+
+    let result = solve TestData.input
+
+module Puzzle8 =
+    let overlap (pair: Pair) =
+        match pair with
+        | (l1, _), (_, r2) when l1 > r2 -> false
+        | (_, r1), (l2, _) when r1 < l2 -> false
+        | _ -> true
+
+    let solve (lines: string array) = 
+        lines
+        |> Array.map parse
+        |> Array.map overlap
+        |> countTrue
+
+    let result = solve TestData.input
--- AdventOfCode2022/Program.fs
+++ AdventOfCode2022/Program.fs
@@ -1,6 +1,7 @@
 ﻿open Day1
 open Day2
 open Day3
+open Day4
 
 printfn "%d" Puzzle1.result
 printfn "%d" Puzzle2.result
@@ -10,3 +11,6 @@ printfn "%d" Puzzle4.result
 
 printfn "%d" Puzzle5.result
 printfn "%d" Puzzle6.result
+
+printfn "%d" Puzzle7.result
+printfn "%d" Puzzle8.result
