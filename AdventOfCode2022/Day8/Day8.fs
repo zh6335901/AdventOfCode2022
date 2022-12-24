53dd0e87647bbcf8423d56648cd5886bb4cab4b0
zh6335901 <zh6335901@hotmail.com>
2022-12-24 13:08:33 +0800

    grids -> trees

--- AdventOfCode2022/Day8/Day8.fs
+++ AdventOfCode2022/Day8/Day8.fs
@@ -5,48 +5,48 @@ open System.IO
 module private TestData = 
     let input = File.ReadAllLines("Day8/input.txt")
     let toDigit c = int c - int '0'
-    let grids = Array2D.init input.Length input[0].Length (fun i j -> input[i][j] |> toDigit)
+    let trees = Array2D.init input.Length input[0].Length (fun i j -> input[i][j] |> toDigit)
 
-let neighbours (grids: int[,]) r c =
+let neighbours (trees: int[,]) r c =
     let left, right = 
-        grids[r, *] 
+        trees[r, *] 
         |> Array.splitAt c 
         |> (fun (l, r) -> (l |> Array.rev, r |> Array.tail))
 
     let above, below = 
-        grids[*, c] 
+        trees[*, c] 
         |> Array.splitAt r 
         |> (fun (a, b) -> (a |> Array.rev, b |> Array.tail))
 
     [ left; right; above; below ]
 
-let isEdge (grids: int[,]) r c =
-    let length1 = (grids |> Array2D.length1)
-    let length2 = (grids |> Array2D.length2)
+let isEdge (trees: int[,]) r c =
+    let length1 = (trees |> Array2D.length1)
+    let length2 = (trees |> Array2D.length2)
 
     r = 0 || c = 0 || r = length1 - 1 || c = length2 - 1
 
 module Puzzle15 =
-    let solve (grids: int[,]) = 
+    let solve (trees: int[,]) = 
         let visible i j v = 
-            match isEdge grids i j with
+            match isEdge trees i j with
             | true -> true
             | false ->
-                neighbours grids i j
+                neighbours trees i j
                 |> Seq.exists (fun ns -> ns |> Seq.forall (fun n -> n < v))
 
-        grids
+        trees
         |> Array2D.mapi (fun i j v -> visible i j v)
         |> Seq.cast<bool>
         |> Seq.filter id
         |> Seq.length
 
-    let result = solve TestData.grids
+    let result = solve TestData.trees
 
 module Puzzle16 = 
-    let solve (grids: int[,]) =  
+    let solve (trees: int[,]) =  
         let score i j v =
-            match isEdge grids i j with
+            match isEdge trees i j with
             | true -> 0
             | false ->
                 let rec grab trees = 
@@ -55,14 +55,14 @@ module Puzzle16 =
                     | t :: ts when t < v -> t :: (grab ts)
                     | t :: _ -> [t]
                 
-                neighbours grids i j
+                neighbours trees i j
                 |> Seq.map (fun ns -> ns |> List.ofArray |> grab)
                 |> Seq.map (fun ns -> (ns |> Seq.length))
                 |> Seq.reduce (*)
 
-        grids
+        trees
         |> Array2D.mapi (fun i j v -> score i j v)
         |> Seq.cast<int>
         |> Seq.max
 
-    let result = solve TestData.grids
+    let result = solve TestData.trees
